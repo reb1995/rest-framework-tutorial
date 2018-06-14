8c36f03886706034a1c17b22beb06c0debf62bed
Ryan Brown <ryan.brown@sas.com>
2018-06-14 13:51:46 -0400

    End of part 7 and the end of the tutorial!

--- snippets/urls.py
+++ snippets/urls.py
@@ -7,8 +7,16 @@ router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title='Pastebin API')
+
+
 # The API URLs are now determined automatically by the router.
 
 urlpatterns = [
-    url(r'^', include(router.urls))
+    url(r'^', include(router.urls)),
+    url(r'^schema/$', schema_view)
 ]
+
+
